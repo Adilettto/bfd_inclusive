2b2941148277faea65da81c46ea9c200f9307021
Adilet <adilet.zamirbekov.kg@gmail.com>
2023-09-13 15:21:42 -0500

    added styles to markup

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import "./Main.scss";
+import styles from "./Main.module.scss";
 import gasBox from "../assets/Gas-box.png";
 
 const Main = () => {
   return (
-    <div className="main">
-      <h1 className="main-text">Fuel now, Pay later No Monthly Fee!</h1>
-      <div className="main-btns">
-        <button className="request">Request a card info</button>
-        <button className="apply">Apply for a card</button>
-        <p>
+    <div className={styles.main}>
+      <h1 className={styles["main-text"]}> <span className={styles["main-text-first"]}>Fuel now</span>, Pay later No Monthly Fee!</h1>
+      <div className={styles["main-btns"]}>
+        <button className={` ${styles["main-btn"]} ${styles["request-btn"]} `}>Request a card info</button>
+        <button className={` ${styles["main-btn"]} ${styles["apply-btn"]} `}>Apply for a card</button>
+        <p className={styles["p-text"]}>
           We offer wide range of fule cards for various types of machinery and
           equipment.
         </p>
-        <img className="main-img" src={gasBox} alt="fuel-box" />
+        
       </div>
+      <img className={styles["main-img"]} src={gasBox} alt="fuel-box" />
     </div>
   );
 };

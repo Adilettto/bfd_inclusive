4062dca1f4759f6501374b33b48315aa2fc5f5ce
Adilet <adilet.zamirbekov.kg@gmail.com>
2023-09-13 23:29:45 -0500

    made markup and styled

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Exports from "../components/Exports";
+import GetDiscount from "../components/GetDiscount";
 
 const Homepage = () => {
   return (
     <div>
+      <GetDiscount />
       <Exports />
     </div>
   );

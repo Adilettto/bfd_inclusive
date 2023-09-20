f70f36df60d020ff24edb16326364748f94bced5
Adilet Zamirbekov <adiletto@Adilets-MacBook-Pro.local>
2023-09-19 23:35:11 -0500

    changed css style

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,19 +4,18 @@ import gasBox from "../assets/Gas-box.png";
 
 const Main = () => {
   return (
-    // TODO styles classNames,
     <div className={styles.main}>
-      <h1 className={styles["main-text"]}> <span className={styles["main-text-first"]}>Fuel now</span>, Pay later No Monthly Fee!</h1>
-      <div className={styles["main-btns"]}>
-        <button className={` ${styles["main-btn"]} ${styles["request-btn"]} `}>Request a card info</button>
-        <button className={` ${styles["main-btn"]} ${styles["apply-btn"]} `}>Apply for a card</button>
-        <p className={styles["p-text"]}>
+      <h1 className={styles.main_text}> <span className={styles.main_textFirst}>Fuel now</span>, Pay later No Monthly Fee!</h1>
+      <div className={styles.main_btns}>
+        <button className={` ${styles.main_btn} ${styles.main_requestBtn} `}>Request a card info</button>
+        <button className={` ${styles.main_btn} ${styles.main_applyBtn} `}>Apply for a card</button>
+        <p className={styles.main_pText}>
           We offer wide range of fule cards for various types of machinery and
           equipment.
         </p>
-
+        
       </div>
-      <img className={styles["main-img"]} src={gasBox} alt="fuel-box" />
+      <img className={styles.main_img} src={gasBox} alt="fuel-box" />
     </div>
   );
 };

9ecc0357e04868bc3c5a1dbde695a70191a8b32b
SyrgakRavshanbekUulu <syrgakravshanbekuulu@gmail.com>
2023-09-20 10:38:39 +0600

    code review 20.09

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,6 +5,7 @@ import gasBox from "../assets/Gas-box.png";
 const Main = () => {
   return (
     <div className={styles.main}>
+      {/* TODO: main__text */}
       <h1 className={styles.main_text}> <span className={styles.main_textFirst}>Fuel now</span>, Pay later No Monthly Fee!</h1>
       <div className={styles.main_btns}>
         <button className={` ${styles.main_btn} ${styles.main_requestBtn} `}>Request a card info</button>
@@ -13,7 +14,7 @@ const Main = () => {
           We offer wide range of fule cards for various types of machinery and
           equipment.
         </p>
-        
+
       </div>
       <img className={styles.main_img} src={gasBox} alt="fuel-box" />
     </div>

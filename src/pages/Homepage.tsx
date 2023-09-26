bd897385188be066699970d932b382bf37688baf
SyrgakRavshanbekUulu <syrgakravshanbekuulu@gmail.com>
2023-09-26 10:41:17 +0600

    containers

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,11 +5,14 @@ import ContactUs from "../components/ContactUs/ContactUs";
 import NavBar from "../components/NavBar/NavBar";
 import Advantage from "../components/Advantage/Advantage";
 import AboutCards from "../components/AboutCards/AboutCards";
+import Main from "../components/Main";
+import styles from './HomePage.module.scss';
 
 const Homepage = () => {
   return (
-    <div>
+    <div className={styles.home}>
       <NavBar />
+      <Main />
       <Advantage />
       <GetDiscount />
       <AboutCards />

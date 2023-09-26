0ec4508faa57f1bd36a82507c72f19a7ba7a3f70
SyrgakRavshanbekUulu <syrgakravshanbekuulu@gmail.com>
2023-09-26 10:29:20 +0600

    fix: components

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import GetDiscount from "../components/GetDiscount/GetDiscount";
 import Exports from "../components/Exports/Exports";
+import ContactUs from "../components/ContactUs/ContactUs";
+import NavBar from "../components/NavBar/NavBar";
+import Advantage from "../components/Advantage/Advantage";
+import AboutCards from "../components/AboutCards/AboutCards";
 
 const Homepage = () => {
   return (
     <div>
+      <NavBar />
+      <Advantage />
       <GetDiscount />
+      <AboutCards />
       <Exports />
+      <ContactUs />
     </div>
   );
 };
